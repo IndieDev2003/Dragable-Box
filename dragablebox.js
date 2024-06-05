c87441b0e662f390f7a049f230be48b5c897c59d
Gagan Suman <ilovescience584@gmail.com>
2024-06-05 09:00:49 +0530

    Design Changes

--- dragablebox.js
+++ dragablebox.js
@@ -2,20 +2,21 @@ const btn = document.querySelector("#btn");
 let textBoxContent = document.querySelector("#text-box");
 let pendingBox = document.querySelector("#left");
 let doneBox = document.getElementById("right");
+let main = document.querySelector(".main")
 
 function AddTask() {
     if (textBoxContent.innerHTML === '') {
         alert("Write Something First!");
     } else {
         console.log("Success");
-        console.log(textBoxContent.value);
+        console.log(textBoxContent.innerHTML);
         let newTask = document.createElement('div');
         newTask.setAttribute("draggable", "true");
         newTask.className = 'task';
         newTask.innerHTML = textBoxContent.innerHTML;
         pendingBox.append(newTask);
         addDragAndDropHandlers(newTask);
-
+        textBoxContent.innerHTML=''
     }
 }
 
@@ -40,11 +41,22 @@ function addDragAndDropHandlers(task) {
             pendingBox.appendChild(selected);
             selected = null;
         });
-    });
-}
 
+    });
+  
+}
 
 let lists = document.getElementsByClassName("task");
 for (let list of lists) {
     addDragAndDropHandlers(list);
 }
+
+function saveData(){
+    localStorage.setItem("data",main.innerHTML)
+}
+function showData(){
+    main.innerHTML=localStorage.getItem("data")
+}
+
+
+
