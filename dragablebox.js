b563367afa954326c0fbb51c297712a795be33a8
Gagan Suman <ilovescience584@gmail.com>
2024-06-05 08:16:20 +0530

    2nd Build

--- dragablebox.js
+++ dragablebox.js
@@ -1,18 +1,50 @@
-const btn =document.querySelector("#btn")
-let textBoxContent=document.querySelector("#text-box")
-let pendingBox=document.querySelector("#left")
-function AddTask(){
-    if(textBoxContent.innerHTML===''){
-        alert("Write Something First !")
+const btn = document.querySelector("#btn");
+let textBoxContent = document.querySelector("#text-box");
+let pendingBox = document.querySelector("#left");
+let doneBox = document.getElementById("right");
+
+function AddTask() {
+    if (textBoxContent.innerHTML === '') {
+        alert("Write Something First!");
+    } else {
+        console.log("Success");
+        console.log(textBoxContent.value);
+        let newTask = document.createElement('div');
+        newTask.setAttribute("draggable", "true");
+        newTask.className = 'task';
+        newTask.innerHTML = textBoxContent.innerHTML;
+        pendingBox.append(newTask);
+        addDragAndDropHandlers(newTask);
+
     }
-    else{
-        console.log("Success")
-        console.log(textBoxContent.innerHTML)
-        let newTask= document.createElement('p')
-        newTask.setAttribute("draggable","true")
-        newTask.className='task'
-        newTask.innerHTML=textBoxContent.innerHTML
-        pendingBox.append(newTask)
-        
-    }
+}
+
+function addDragAndDropHandlers(task) {
+    task.addEventListener("dragstart", function(e) {
+        let selected = e.target;
+
+        doneBox.addEventListener("dragover", function(e) {
+            e.preventDefault();
+        });
+
+        doneBox.addEventListener("drop", function() {
+            doneBox.appendChild(selected);
+            selected = null;
+        });
+
+        pendingBox.addEventListener("dragover", function(e) {
+            e.preventDefault();
+        });
+
+        pendingBox.addEventListener("drop", function() {
+            pendingBox.appendChild(selected);
+            selected = null;
+        });
+    });
+}
+
+
+let lists = document.getElementsByClassName("task");
+for (let list of lists) {
+    addDragAndDropHandlers(list);
 }
